41915c0ff901e758d39a444e81de4a0cd2c2be02
Mason Nugent <masoncnugent@gmail.com>
2023-01-02 20:11:43 -0800

    automated_librarian_script.py now takes user input and shows the appropriate book linked list

--- automated_librarian_script.py
+++ automated_librarian_script.py
@@ -20,11 +20,30 @@ def insert_book_data():
         for book in book_data:
             if book[0] == book_type:
                 book_sublist.insert_beginning(book)
-        print("stringifying book_sublist")
-        print(book_sublist.stringify_list())
         book_data_list.insert_beginning(book_sublist)
+
     return book_data_list
 
-insert_book_types()
+def get_user_choices():
+    print("see if you can abstract some choice gathering to here")
 
-insert_book_data()
+def run_program():
+    #this book_type_list isn't actually in use yet...
+    #insert_book_types()
+    book_data_list = insert_book_data()
+
+    user_letter_choice = input("What type of book would you like to read?\nType \'p\' for philosophical, \'c\' for comic strip, \'a\' for adventure, or \'e\' for english curriculum\n")
+
+    #this determines what genre the user has chosen
+    current_list = book_data_list.get_head_node()
+    while current_list.get_value():
+        current_genre = current_list.get_value().get_head_node().get_value()[0]
+        if user_letter_choice == current_genre[0]:
+            print("\nHere is a list of " + current_genre + " books:")
+            print(current_list.get_value().stringify_list())
+            break
+        else:
+            current_list = current_list.get_next_node()
+
+
+run_program()
