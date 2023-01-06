6e02ecdec836109fe592de935019e24ee581d8d6
Mason Nugent <masoncnugent@gmail.com>
2023-01-06 00:18:34 -0800

    Cleaned up some of the code and added prefix_checker.

--- automated_librarian_script.py
+++ automated_librarian_script.py
@@ -2,18 +2,8 @@ from books import *
 from welcome import *
 from linkedlist import LinkedList
 
-#print_welcome()
 
-#makes a linkedlist of all the book types
-def insert_book_types():
-    book_type_list = LinkedList()
-    for book_type in types:
-        book_type_list.insert_beginning(book_type)
-    print(book_type_list.stringify_list())
-    print("done with this section\n")
-    return book_type_list
-
-#creates the linkedlist of linkedlists with book types as the sublist lists
+#creates the linkedlist of linkedlists with book types as the sublists
 def insert_book_data():
     book_data_list = LinkedList()
     for book_type in types:
@@ -26,11 +16,7 @@ def insert_book_data():
     return book_data_list
 
 
-def get_user_choices():
-    pass
-
-
-#this determines what genre the user has chosen and prints all its books to the console.
+#this determines what genre the user has chosen and prints all its books to the console
 def genre_printer(book_data_list, user_letter_choice):
     current_list = book_data_list.get_head_node()
     while current_list.get_value():
@@ -44,6 +30,7 @@ def genre_printer(book_data_list, user_letter_choice):
             current_list = current_list.get_next_node()
 
 
+#creates a sentence of the book types with an oxford comma at the end
 def book_type_oxford(types):
     oxford_sentence = ""
     pointer = 0
@@ -53,57 +40,76 @@ def book_type_oxford(types):
     oxford_sentence += "and " + types[pointer] + "."
     return oxford_sentence
 
+#looks through types and makes a list of all the letter prefixes of the types. this might be refactored entirely so strings of starting characters of the types can be compared, as a more sophisticated search
 def book_type_prefixes(types):
     prefix_list = []
     pointer = 0
     while pointer != len(types):
         prefix_list.append(types[pointer][0])
+        pointer += 1
     return prefix_list
 
+#returns True if the user chose a letter that is one of the prefixes within the types list
+def prefix_checker(user_letter_choice, prefix_list):
+    if user_letter_choice in prefix_list:
+        return True
+    return False
 
-def main_loop(book_data_list):
+#calls the helper functions and contains the core logic for when those various functions are called
+def main_loop():
+    book_data_list = insert_book_data()
+
     run_state = 0
-    user_reset_choice = False
+    user_reset_choice = "y"
     user_reset_choice_question = "Would you like to search for more books? Type \'y\' for yes or \'n\' for no.\n"
-    while not user_reset_choice:
+    while user_reset_choice == "y":
         if run_state == 0:
             user_letter_choice = input("What type of books would you like to read?\nType the first letters of your choosing, with your options being {0}\n".format(book_type_oxford(types)))
-            #add logic to test if user_letter_choice is actually in the book_type_prefixes(types). You could make this smarter so it combs through the start of each type to see which type it has the most matches for...
+            if prefix_checker(user_letter_choice, book_type_prefixes(types)):
+                genre_printer(book_data_list, user_letter_choice)
+            else:
+                while not prefix_checker(user_letter_choice, book_type_prefixes(types)):
+                    user_letter_choice = input("\nI'm sorry, it appears you've made a incorrect entry.\nType the first letters of your choosing, with your options being {0}\n".format(book_type_oxford(types)))
+                genre_printer(book_data_list, user_letter_choice)
 
-            genre_printer(book_data_list, user_letter_choice)
             user_reset_choice = input(user_reset_choice_question)
-            while user_reset_choice != "y" or user_reset_choice != "n":
+            while user_reset_choice != "y" and user_reset_choice != "n":
                 user_reset_choice = input(user_reset_choice_question)
-                print(user_reset_choice)
-            #figure out how to not need these break statements. they shouldn't be needed...
+            
             if user_reset_choice == "y":
                 run_state = 1
-                break
-            elif user_reset_choice == "n":
-                user_reset_choice = True
-                print(user_reset_choice)
-                break
 
+        #this is the run state where the user has asked to see more books. in this case, the dialogue is different to reflect that change
         elif run_state == 1:
-            user_letter_choice = input("What other type of books would you like to read?\nType the first letters of your choosing, with your options being {0}\n".format(book_type_oxford(types)))
-            genre_printer(book_data_list, user_letter_choice)
+            user_letter_choice = input("\nWhat other type of books would you like to read?\nType the first letters of your choosing, with your options being {0}\n".format(book_type_oxford(types)))
+            if prefix_checker(user_letter_choice, book_type_prefixes(types)):
+                genre_printer(book_data_list, user_letter_choice)
+            else:
+                while not prefix_checker(user_letter_choice, book_type_prefixes(types)):
+                    user_letter_choice = input("\nI'm sorry, it appears you've made a incorrect entry.\nType the first letters of your choosing, with your options being {0}\n".format(book_type_oxford(types)))
+                genre_printer(book_data_list, user_letter_choice)
+
+            user_reset_choice = input(user_reset_choice_question)
+            while user_reset_choice != "y" and user_reset_choice != "n":
+                user_reset_choice = input(user_reset_choice_question)
 
 
-    print("I hope you found the book(s) you were looking for! Have a nice day!")
-
+    print("\nI hope you found the book(s) you were looking for! Have a nice day!")
 
 
 
 
 def run_program():
-    #this book_type_list isn't actually in use yet...
-    #insert_book_types()
+    #print_welcome()
 
-    book_data_list = insert_book_data()
-
-    #do an introductory step and then the main loop
-
-    main_loop(book_data_list)
-    print(book_type_prefixes(types))
+    main_loop()
         
+#additional stuff to add:
+"""
+make it so that the user can select a specific book, which should print out a sentence detailing it's name, author, critic and user reviews.
+you could make it so that searches are true searches with a conflict message asking the user to make a choice if two or more options match the multi-character prefix string the user gave.
+finish print_welcome, of course.
+fill out book_data
+"""
+
 run_program()
